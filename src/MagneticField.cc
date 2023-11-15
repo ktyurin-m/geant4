de8a3150827ea49ec08bc70b55d96de5f5ad8ab9
ktyutrin-m <kirilltiutrin@gmail.com>
2023-11-15 16:57:13 +0700

    update

--- src/MagneticField.cc
+++ src/MagneticField.cc
@@ -24,7 +24,7 @@ namespace project
 
   void MagneticField::GetFieldValue(const G4double point[4], double *bField) const
   {
-    G4double Hm = 2300*gauss;
+    G4double Hm = 10000*gauss;
     bField[0] = 0.;
     bField[1] = -Hm;
     bField[2] = 0;
