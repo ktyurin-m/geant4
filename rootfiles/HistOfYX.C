42aa2c6a8644d69cf488365d7c286c6000e46898
ktyutrin-m <kirilltiutrin@gmail.com>
2023-12-11 17:22:36 +0700

    update

--- rootfiles/HistOfYX.C
+++ rootfiles/HistOfYX.C
@@ -12,7 +12,7 @@
 #include <TFile.h>
 void HistOfYX()
 {
-    TFile *f = new TFile("out4.root");
+    TFile *f = new TFile("out3.root");
     std::cout <<"File name: " << f->GetName() << std::endl;
     TTree *t = (TTree *)f->Get("Data");
     Double_t z;
@@ -28,19 +28,19 @@ void HistOfYX()
 
     TH2F *h1 = new TH2F("h1", "h1", 100, -60, 60, 100, 0, 1900);
 
-    TH2F *h2_1 = new TH2F("gamma", "gamma", 10, -5, 5, 10, -5, 5); // gamma
+    TH2F *h2_1 = new TH2F("gamma", "gamma", 20, -20, 20, 20, -20, 20); // gamma
     TH2F *h2_2 = new TH2F("e-", "e-", 10, -5, 5, 10, -5, 5);       // electron
     TH2F *h2_3 = new TH2F("e+", "e+", 10, -5, 5, 10, -5, 5);       // positron
 //y z gamma
-    TH1F *h3_1 = new TH1F("HistOfEnergy", "HistOfEnergy", 100, -10, 10); // y
-    TH1F *h3_2 = new TH1F("HistOfEnergy", "HistOfEnergy", 100, -10, 10); // 
+    TH1F *h3_1 = new TH1F("", "", 200, -20, 20); // y
+    TH1F *h3_2 = new TH1F("", "", 200, -20, 20); // z
     // TH1F *h4  = new TH1F("h3_1","h3_1",300, -100,100) ;
     Int_t ent = t->GetEntries();
     for (Int_t i = 0; i < ent; i++)
     {
         t->GetEntry(i);
         Double_t R = sqrt(pow(z, 2) + pow(y, 2));
-        if (x >= -2.1 && x <= -1.9)
+        if (x >= -2.1 && x <= -1.9 && R < 15)
         {
             if (strcmp(Particle, "gamma") == 0)
             {
@@ -52,9 +52,11 @@ void HistOfYX()
         }
 
     }
+    TF1 * gaussian = new TF1("gaussian","gaus", -10, 10);
 
     t->ResetBranchAddresses();
     TCanvas *c = new TCanvas;
+    gStyle->SetOptFit();
     c->Divide(3, 1);
     c->cd(1);
     h2_1->SetTitle("Hits");
@@ -63,14 +65,29 @@ void HistOfYX()
     h2_1->Draw("colz");
 
     c->cd(2);
+    gaussian->SetLineWidth(5);
+    h3_1->Fit(gaussian, "R");
     h3_1->SetXTitle("y");
     h3_1->SetYTitle("Event");
     h3_1->Draw();
-
+    gaussian->Draw("same");
     c->cd(3);
+    h3_2->Fit(gaussian, "R");
     h3_2->SetXTitle("z");
     h3_2->SetYTitle("Event");
     h3_2->Draw();
     c->Update();
+
+    //work with data
+    
+    // double hmax = gaussian->GetMaximum();
+    // double halfmax = hmax/2;
+    // double x1, x2;
+    // x1 = gaussian->GetX(halfmax, -12, gaussian->GetParameter(1));
+    // x2 = gaussian->GetX(halfmax, gaussian->GetParameter(1), 12);
+
+    // double sigma = (x2-x1)/(2*sqrt(2*log(2)));
+    cout << "Sigma: " << gaussian->GetParameter(2) << endl;
+
     
 }
---
+++ rootfiles/drawGraphFromDataFile.C
@@ -0,0 +1,12 @@
+void drawGraphFromDataFile() {
+TGraph *graph = new TGraph("data", "%lg %lg"); // создание графика и загрузка данных из файла data.txt
+graph->Draw("APL"); // рисование графика
+TLatex latex;
+
+graph->GetXaxis()->SetTitle("#sigma, mm"); // название оси X
+graph->GetYaxis()->SetTitle("E, MeV"); // название оси Y
+graph->SetMarkerStyle(20);
+gPad->SetGrid(); // включение сетки
+gPad->Modified(); // обновление рисунка
+gPad->Update(); // обновление рисунка
+}
